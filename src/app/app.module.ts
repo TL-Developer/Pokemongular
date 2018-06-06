911d19d2ceb2f761dbdefa576278b804e852ebf3
Tiago Lima <tiagol.ext@vr.com.br>
2018-06-06 14:46:14 -0300

    ajustes de layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,22 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PokemonsComponent } from './pokemons/pokemons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PokemonsComponent
   ],
   imports: [
     BrowserModule,
-    MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule
+    MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatGridListModule, MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pokemons/pokemons.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-pokemons',
+  templateUrl: './pokemons.component.html',
+  styleUrls: ['./pokemons.component.scss']
+})
+export class PokemonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
